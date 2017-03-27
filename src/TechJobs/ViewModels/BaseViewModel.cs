03aa0a7d85e6ed58c7c0e8b7e732c86471082ec1
Kevin Childs <kevinicus37@gmail.com>
2017-03-27 18:37:17 -0500

    Create and inherit from BaseViewModel

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        // View title
+        public string Title { get; set; } = "";
+
+        // All columns, for display
+        public List<JobFieldType> Columns { get; set; }
+
+        // Column to view or search
+        public JobFieldType Column { get; set; }
+
+
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
